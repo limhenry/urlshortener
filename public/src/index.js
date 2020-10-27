5b7c1706db53ce32c04951a3e15d728a37aea0c0
Henry Lim <henrylim.everything@gmail.com>
2020-10-27 22:25:27 +0700

    big update

--- public/src/index.js
+++ public/src/index.js
@@ -1,20 +1,20 @@
 import { Component } from 'preact';
-import Toolbar from 'preact-material-components/Toolbar';
-import 'preact-material-components/Toolbar/style.css';
-import Button from 'preact-material-components/Button';
+import { TopAppBar, Button } from 'preact-material-components';
+import 'preact-material-components/TopAppBar/style.css';
 import 'preact-material-components/Button/style.css';
-import style from './style';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 import Worker from 'worker-loader!./Worker.js';
 import config from './config';
+import './style';
 
 export default class App extends Component {
 
 	state = {
 		baseUrl: config.baseUrl,
-		data: []
+		data: [],
+		user: false
 	}
 
 	constructor() {
@@ -23,6 +23,7 @@ export default class App extends Component {
 		firebase.initializeApp(config.firebase);
 		firebase.auth().onAuthStateChanged((user) => {
 			if (user) {
+				this.setState({ user });
 				document.getElementById('loading').style.display = 'none';
 				document.getElementById('home').style.display = 'block';
 				this.showToast('Loading ...', 0);
@@ -48,10 +49,13 @@ export default class App extends Component {
 		let data = [];
 		this.db.collection('url').orderBy('timestamp', 'desc').get().then(querySnapshot => {
 			querySnapshot.forEach(doc => {
-				data.push(doc.data());
+				const d = doc.data();
+				d.id = doc.id;
+				d.timestamp = d.timestamp.toDate();
+				data.push(d);
 			});
 			worker.postMessage(data);
-		}).catch(error => {
+		}).catch(() => {
 			this.showToast('Something went wrong. Please refresh the page.');
 		});
 	}
@@ -82,6 +86,7 @@ export default class App extends Component {
 						timestamp: new Date()
 					}).then(() => {
 						this.getData();
+						this.showToast('Added successfully.');
 					}).catch(error => {
 						this._toastClosed();
 						this.showToast('Something went wrong. Please try again.');
@@ -170,16 +175,36 @@ export default class App extends Component {
 		}
 	}
 
-	render({ }, { data, baseUrl }) {
+	deleteUrl = (e) => () => {
+		const msg = `This action cannot be undone. Are you sure you want to delete ${this.state.baseUrl}/${e.shorturl}?`;
+		if (!window.confirm(msg)) return;
+		this.db.collection('url').doc(e.id).delete()
+			.then(() => {
+				this.showToast('Delete successful.');
+				this.getData();
+			})
+			.catch((e) => {
+				this.showToast('Unable to delete URL. Please try again.');
+			});
+	}
+
+	render({ }, { data, baseUrl, user }) {
 		return (
-			<div>
-				<Toolbar class={style.toolbar}>
-					<Toolbar.Row>
-						<Toolbar.Section align-start>
-							<Toolbar.Title>URL Shortener</Toolbar.Title>
-						</Toolbar.Section>
-					</Toolbar.Row>
-				</Toolbar>
+			<div class="app">
+				<TopAppBar className="toolbar mdc-top-app-bar--fixed">
+					<TopAppBar.Row>
+						<TopAppBar.Section align-start>
+							<TopAppBar.Title>URL Shortener</TopAppBar.Title>
+						</TopAppBar.Section>
+						{ user && (
+							<TopAppBar.Section align-end>
+								<div class="profile">
+									<img src={user.photoURL} title={`${user.displayName} · ${user.email}`} />
+								</div>
+							</TopAppBar.Section>
+						)}
+					</TopAppBar.Row>
+				</TopAppBar>
 				<div id="toast" class="toast">Loading ...</div>
 				<div id="loading" class="loading">
 					Loading ...
@@ -195,7 +220,7 @@ export default class App extends Component {
 								<input type="url" id="shorturl" onkeydown={this.shortUrlInput} onfocus={this.shortUrlInput} onfocusout={this.shortUrlInput} placeholder="Your custom short URL here (Optional)" />
 								<Button ripple raised id="button" onClick={this.shortenUrl}>Shorten URL</Button>
 							</div>
-							<p>All {baseUrl} URLs are public and can be accessed by anyone</p>
+							<p>All <b>{baseUrl}</b> URLs are public and can be accessed by anyone.</p>
 						</div>
 					</div>
 					<div class="content">
@@ -206,15 +231,28 @@ export default class App extends Component {
 									<th class="created">Created</th>
 									<th class="short">Short URL</th>
 									<th class="clicks">All Clicks</th>
+									<th class="manage">Manage</th>
 								</tr>
 							</thead>
 							<tbody>
 								{data.map((item, i) => (
 									<tr>
-										<td class="original"><a target="_blank" rel="noreferrer" href={item.fullurl}>{item.fullurl}</a></td>
-										<td class="created">{item.created}</td>
-										<td class="short"><a target="_blank" rel="noreferrer" href={'http://' + baseUrl + '/' + item.shorturl}>{baseUrl}/{item.shorturl}</a></td>
+										<td class="original">
+											<a target="_blank" rel="noreferrer"  href={item.fullurl} title={item.fullurl}>{item.fullurl}</a>
+										</td>
+										<td class="created">{item.timestamp.toDateString()}</td>
+										<td class="short">
+											<a target="_blank" rel="noreferrer" href={'https://' + baseUrl + '/' + item.shorturl}>{baseUrl}/{item.shorturl}</a>
+										</td>
 										<td class="clicks">{item.count}</td>
+										<td class="manage">
+											<div class="delete" onClick={this.deleteUrl(item)}>
+												<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+													<path d="M0 0h24v24H0V0z" fill="none" />
+													<path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v10zM9 9h6c.55 0 1 .45 1 1v8c0 .55-.45 1-1 1H9c-.55 0-1-.45-1-1v-8c0-.55.45-1 1-1zm6.5-5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1h-2.5z" />
+												</svg>
+											</div>
+										</td>
 									</tr>
 								))}
 							</tbody>
@@ -224,6 +262,7 @@ export default class App extends Component {
 									<th class="created">Created</th>
 									<th class="short">Short URL</th>
 									<th class="clicks">All Clicks</th>
+									<th class="manage">Manage</th>
 								</tr>
 							</thead>
 						</table>
